507f9a3346ac2777516996d4a63838dc06841e38
lenny-zanotelli <lenny.zanotelli24@gmail.com>
2024-09-24 12:14:59 +0200

    fix answer with user2

--- typescript-basic/challenge.ts
+++ typescript-basic/challenge.ts
@@ -2,7 +2,7 @@
 
 interface User {
   name: string;
-  age?: number;
+  age: number;
   birthday?: string;
 }
 
@@ -16,7 +16,7 @@ const wilders: User[] = [];
 
 const user1 = { name: "Pierre", age: 23 };
 
-const user2 = { name: "Paul", birthday: "10/02/1990" };
+const user2 = { name: "Paul", age: 34, birthday: "10/02/1990" };
 
 const user3 = { name: "Jacques", age: 25 };
 
@@ -26,4 +26,4 @@ wilders.push(user2);
 
 wilders.push(user3);
 
-prettyPrintWilder(wilders);
+console.log(prettyPrintWilder(wilders));
