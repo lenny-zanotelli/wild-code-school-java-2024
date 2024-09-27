2b5607f3ffcfd54ae0804d58326d966dfc5dba08
lenny-zanotelli <lenny.zanotelli24@gmail.com>
2024-09-27 11:36:19 +0200

    create catalog component

--- angular-01/wild-blog/src/app/app.component.ts
+++ angular-01/wild-blog/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ArticleComponentComponent } from './article-component/article-component.component';
 import { MonComposantComponent } from './mon-composant/mon-composant.component';
+import { CatalogComponent } from "./catalog/catalog.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ArticleComponentComponent, MonComposantComponent],
+  imports: [RouterOutlet, ArticleComponentComponent, MonComposantComponent, CatalogComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
