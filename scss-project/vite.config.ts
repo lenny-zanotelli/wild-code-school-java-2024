938a1db2afc0c0028d373dcb26a1ed8d3029a6d6
lenny-zanotelli <lenny.zanotelli24@gmail.com>
2024-10-02 09:32:52 +0200

    fix: config vite and import script html

--- scss-project/vite.config.ts
+++ scss-project/vite.config.ts
@@ -1,7 +1,7 @@
 import type { UserConfig } from "vite";
 
 export default {
-  base: "/wild-code-school-java-2024",
+  base: "/wild-code-school-java-2024/",
   css: {
     preprocessorOptions: {
       scss: {
