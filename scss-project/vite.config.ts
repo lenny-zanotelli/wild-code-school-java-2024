e9fc63ffc996529ea0607cf27328e174d2cb8ec8
lenny-zanotelli <lenny.zanotelli24@gmail.com>
2024-10-02 10:15:06 +0200

    fix: explicit outDir vite config

--- scss-project/vite.config.ts
+++ scss-project/vite.config.ts
@@ -2,6 +2,9 @@ import type { UserConfig } from "vite";
 
 export default {
   base: "/wild-code-school-java-2024/scss-project/",
+  build: {
+    outDir: "./dist",
+  },
   css: {
     preprocessorOptions: {
       scss: {
